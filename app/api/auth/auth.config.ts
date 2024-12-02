8d4cf0eec892a52a73d1b327e77b0e06d6ae89cb
caiwenchao <caiwenchao@corp.netease.com>
2024-12-02 17:15:44 +0800

    删除掉不再使用的import语句。

--- app/api/auth/auth.config.ts
+++ app/api/auth/auth.config.ts
@@ -3,7 +3,6 @@ import GithubProvider from "next-auth/providers/github"
 import GoogleProvider from "next-auth/providers/google"
 import { MongoDBAdapter } from "@auth/mongodb-adapter"
 import clientPromise from "../../../lib/mongodb"
-import { authConfig } from "../../../lib/auth-config"
 
 declare module "next-auth" {
   interface Session {
